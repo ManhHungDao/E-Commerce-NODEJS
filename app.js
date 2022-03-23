a403c0508ddfffb75b99baff40ebd27522a2017f
hung dao <hungpepi2001@gmail.com>
2022-03-24 01:08:32 +0700

    new fix

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ mongoose
     .catch((err) => {
         console.log(err);
     });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 //Server
 app.listen(PORT, () => {
     console.log('server is running http://localhost:3000');
